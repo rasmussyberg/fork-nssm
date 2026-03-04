2354e188dae76d366e1caa557847c41c58c1e000
Rasmus Syberg <rasmussyberg@gmail.com>
2026-03-04 14:35:57 +0100

    chore: version update

--- version.h
+++ version.h
@@ -1,5 +1,5 @@
-#define NSSM_VERSION _T("0.0")
+#define NSSM_VERSION _T("2.25")
 #define NSSM_VERSIONINFO 0,0,0,0
-#define NSSM_DATE _T("20-01-2026")
+#define NSSM_DATE _T("04-03-2026")
 #define NSSM_FILEFLAGS VS_FF_PRERELEASE
 #define NSSM_COPYRIGHT _T("Public Domain; Author Iain Patterson 2003-")
